44bf3d55c51322222ef5fa5fc8a558c568a78d30
unknown <benedictusaryo@outlook.com>
2019-11-08 18:42:15 +0700

    add arguments and extension

--- face_detection.py
+++ face_detection.py
@@ -8,10 +8,45 @@ Created on Wed Oct  16 10:14:14 2019
 #######################################################################
 ######################  Library Initialization  #########################
 #  Import Library being used in program
-from openvino.inference_engine import IENetwork, IEPlugin
-import cv2 as cv
-import platform
 import time
 import argparse
+import platform
+if __name__ == '__main__':
+    import os
+    os.system('setupvars')
+    from openvino.inference_engine import IENetwork, IEPlugin
+    import cv2 as cv
 
 #####################  Argument Parser  ################################
+parser = argparse.ArgumentParser(description="OpenVINO Face Detection")
+parser.add_argument("-d", "--device", metavar='', default='CPU',
+                    help="Device to run inference: GPU, CPU or MYRIAD", type=str)
+parser.add_argument("-c", "--camera", metavar='', default=0,
+                    help="Camera Device, default 0 for Webcam", type=int)
+parser.add_argument("-s", "--sample", default=False,
+                    action='store_true', help="Inference using sample video")
+
+args = parser.parse_args()
+
+#######################  Device  Initialization  ########################
+#  Plugin initialization for specified device and load extensions library if specified
+
+device = args.device.upper()
+
+# Device Options = "CPU", "GPU", "MYRIAD"
+plugin = IEPlugin(device=device)
+
+# DETECT OS WINDOWS / UBUNTU  TO USE EXTENSION LIBRARY
+# Plugin UBUNTU :
+LINUX_CPU_PLUGIN = "/opt/intel/openvino/deployment_tools/inference_engine/lib/intel64/libcpu_extension_avx2.so"
+# Plugin Windows
+WINDOWS_CPU_PLUGIN = r"C:\Program Files (x86)\IntelSWTools\openvino\deployment_tools\inference_engine\bin\intel64\Release\cpu_extension_avx2.dll"
+
+if platform.system() == 'Windows':
+    cpu_plugin = WINDOWS_CPU_PLUGIN
+else:
+    cpu_plugin = LINUX_CPU_PLUGIN
+
+# Add Extension to Device Plugin
+if device == "CPU":
+    plugin.add_cpu_extension(cpu_plugin)
